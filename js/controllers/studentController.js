dcdb6b8914fd63126cc07a999501669d68559b65
Tevin Mantock <tmantock@gmail.com>
2016-06-19 16:57:37 -0700

    added ng-click and ng-show to hide or show relevant student data

--- js/controllers/studentController.js
+++ js/controllers/studentController.js
@@ -1,5 +1,37 @@
 app.controller("studentController", ["studentTableService", function(studentTableService){
   var self = this;
+  self.headerOne = "Student Name";
+  self.headerTwo = "Grade Level";
+  self.headerThree = "Grade Point Average";
   self.students = studentTableService.students;
-  console.log(self.students);
+  self.guardianList = true;
+  self.courseList = true;
+
+  self.guardianExpand = function () {
+    if(self.guardianList && self.courseList){
+      self.headerOne = "Guardian";
+      self.headerTwo = "Guardian Name";
+      self.headerThree = "Guardian Contact";
+      self.guardianList = false;
+    }else{
+      self.headerOne = "Student Name";
+      self.headerTwo = "Grade Level";
+      self.headerThree = "Grade Point Average";
+      self.guardianList = true;
+    }
+  };
+
+  self.courseExpand = function () {
+    if(self.courseList && self.guardianList){
+      self.headerOne = "Instructor";
+      self.headerTwo = "Course";
+      self.headerThree = "Grade";
+      self.courseList = false;
+    }else{
+      self.headerOne = "Student Name";
+      self.headerTwo = "Grade Level";
+      self.headerThree = "Grade Point Average";
+      self.courseList = true;
+    }
+  };
 }]);
