3118367ec14867e01d637f6f0359438c703ed9c0
Tevin Mantock <tmantock@gmail.com>
2016-07-17 00:04:42 -0700

    comments added to js

--- js/directives/studentInfo.js
+++ js/directives/studentInfo.js
@@ -1,30 +1,9 @@
+//student input directive for storing the html template for the student input form
 app.directive("studentInfo", function() {
     return {
         restrict: 'E',
         scope: true,
         templateUrl: 'js/directives/studentInfo.html',
-        link: function($scope, $element, $attr) {
-          angular.element($element).append($scope[$attr.ngModel]);
-    }
-    };
-});
-
-app.directive("guardianInfo", function() {
-    return {
-        restrict: 'E',
-        scope: true,
-        templateUrl: 'js/directives/guardianInfo.html',
-        link: function($scope, $element, $attr) {
-          angular.element($element).append($scope[$attr.ngModel]);
-    }
-    };
-});
-
-app.directive("courseInfo", function() {
-    return {
-        restrict: 'E',
-        scope: true,
-        templateUrl: 'js/directives/courseInfo.html',
         link: function($scope, $element, $attr) {
           angular.element($element).append($scope[$attr.ngModel]);
     }
